7d93df6f90992f4aaff3b3fd6f78e820ff175b52
Hennie <henniehjvanheerden@gmail.com>
2024-11-06 21:26:23 +0200

    Logic updates. Added exponentiation for largge numbers. Fixed a bug with max limit of numbers. = now calls operate(). To do : Extra Credit

--- calculator.js
+++ calculator.js
@@ -2,6 +2,7 @@
 let number1 = '';
 let number2 = '';
 let currentFunction = '';
+let result = '';
 //Number swop variable
 let activeNumber = 1;
 //Control variables
@@ -11,22 +12,76 @@ const clearButton = document.querySelector('#clearButton');
 const clearEverythingButton = document.querySelector('#clearEverythingButton');
 //Setting initial display value
 outputString.textContent = 0;
+//Calculate result
+const operate = (num1, num2, func) => {
+
+    switch(func){
+        case "+":
+            result = String(Number(num1)+Number(num2));
+            activeNumber = 1;
+            if(result.length > 10){
+                result = Number(result).toExponential(3);
+            }
+            outputString.textContent = result;
+            number1 = '';
+            number2 = '';
+            currentFunction ='';
+            break;
+        case "-":
+            result = String(Number(num1)-Number(num2));
+            activeNumber = 1;
+            outputString.textContent = result;
+            if(result.length > 10){
+                result = Number(result).toExponential(3);
+            }
+            number1 = '';
+            number2 = '';
+            currentFunction ='';
+            break;
+        case "/":
+            result = String(Number(num1)/Number(num2));
+            activeNumber = 1;
+            if(result.length > 10){
+                result = Number(result).toExponential(3);
+            }
+            outputString.textContent = result;
+            number1 = '';
+            number2 = '';
+            currentFunction ='';
+            break;
+        case "*":
+            result = String(Number(num1)*Number(num2));
+            activeNumber = 1;
+            if(number2 == '0' || number2 == ''){
+                outputString.textContent = Nan;
+                number1 = '';
+                number2 = '';
+                currentFunction ='';
+            }else
+            if(result.length > 10){
+                result = Number(result).toExponential(3);
+            }
+            outputString.textContent = result;
+            number1 = '';
+            number2 = '';
+            currentFunction ='';
+            break;              
+    }
+}
 
 //Events
 const processInput = (e) => {
 
     const event = e.target;
-    console.log(event);
-
     switch(event.id){
         case '1':
             if(activeNumber == 1){
-                if(outputString.textContent.length <= 18){
+                if(number1.length <= 18){
                     number1 = number1 + '1';
                     outputString.textContent = number1;} 
                     else break;
             }else{
-                if(outputString.textContent.length <= 18){
+                if(number2.length <= 18){
                     number2 = number2 + '1';
                     outputString.textContent = number2;}
                     else break;
@@ -34,12 +89,12 @@ const processInput = (e) => {
             break;
         case '2':
             if(activeNumber == 1){
-                if(outputString.textContent.length <= 18){
+                if(number1.length <= 18){
                     number1 = number1 + '2';
                     outputString.textContent = number1;}
                     else break;
             }else{
-                if(outputString.textContent.length <= 18){
+                if(number2.length <= 18){
                     number2 = number2 + '2';
                     outputString.textContent = number2;}
                     else break;
@@ -47,12 +102,12 @@ const processInput = (e) => {
             break;
         case '3':
             if(activeNumber == 1){
-                if(outputString.textContent.length <= 18){
+                if(number1.length <= 18){
                     number1 = number1 + '3';
                     outputString.textContent = number1;}
                     else break;
             }else{
-                if(outputString.textContent.length <= 18){
+                if(number2.length <= 18){
                     number2 = number2 + '3';
                     outputString.textContent = number2;}
                     else break;
@@ -60,12 +115,12 @@ const processInput = (e) => {
             break;
         case '4':
             if(activeNumber == 1){
-                if(outputString.textContent.length <= 18){
+                if(number1.length <= 18){
                     number1 = number1 + '4';
                     outputString.textContent = number1;}
                     else break;
             }else{
-                if(outputString.textContent.length <= 18){
+                if(number2.length <= 18){
                     number2 = number2 + '4';
                     outputString.textContent = number2;}
                     else break;
@@ -73,12 +128,12 @@ const processInput = (e) => {
             break;
         case '5':
             if(activeNumber == 1){
-                if(outputString.textContent.length <= 18){
+                if(number1.length <= 18){
                     number1 = number1 + '5';
                     outputString.textContent = number1;}
                     else break;
             }else{
-                if(outputString.textContent.length <= 18){
+                if(number2.length <= 18){
                     number2 = number2 + '5';
                     outputString.textContent = number2;}
                     else break;
@@ -86,12 +141,12 @@ const processInput = (e) => {
             break;
         case '6':
             if(activeNumber == 1){
-                if(outputString.textContent.length <= 18){
+                if(number1.length <= 18){
                     number1 = number1 + '6';
                     outputString.textContent = number1;}
                     else break;
             }else{
-                if(outputString.textContent.length <= 18){
+                if(number2.length <= 18){
                     number2 = number2 + '6';
                     outputString.textContent = number2;}
                     else break;
@@ -99,12 +154,12 @@ const processInput = (e) => {
             break;
         case '7':
             if(activeNumber == 1){
-                if(outputString.textContent.length <= 18){
+                if(number1.length <= 18){
                     number1 = number1 + '7';
                     outputString.textContent = number1;}
                     else break;
             }else{
-                if(outputString.textContent.length <= 18){
+                if(number2.length <= 18){
                     number2 = number2 + '7';
                     outputString.textContent = number2;}
                     else break;
@@ -112,12 +167,12 @@ const processInput = (e) => {
             break;
         case '8':
             if(activeNumber == 1){
-                if(outputString.textContent.length <= 18){
+                if(number1.length <= 18){
                     number1 = number1 + '8';
                     outputString.textContent = number1;}
                     else break;
             }else{
-                if(outputString.textContent.length <= 18){
+                if(number2.length <= 18){
                     number2 = number2 + '8';
                     outputString.textContent = number2;}
                     else break;
@@ -125,12 +180,12 @@ const processInput = (e) => {
             break;
         case '9':
             if(activeNumber == 1){
-                if(outputString.textContent.length <= 18){
+                if(number1.length <= 18){
                     number1 = number1 + '9';
                     outputString.textContent = number1;}
                     else break;
             }else{
-                if(outputString.textContent.length <= 18){
+                if(number2.length <= 18){
                     number2 = number2 + '9';
                     outputString.textContent = number2;}
                     else break;
@@ -138,12 +193,12 @@ const processInput = (e) => {
             break;
         case '0':
             if(activeNumber == 1){
-                if(outputString.textContent.length <= 18){
+                if(number1.length <= 18){
                     number1 = number1 + '0';
                     outputString.textContent = number1;}
                     else break;
             }else{
-                if(outputString.textContent.length <= 18){
+                if(number2.length <= 18){
                     number2 = number2 + '0';
                     outputString.textContent = number2;}
                     else break;
@@ -151,84 +206,86 @@ const processInput = (e) => {
             break;
         case '.':
             if(activeNumber == 1){
-                if(outputString.textContent.length <= 18){
+                if(number1.length <= 18){
                     number1 = number1 + '.';
                     outputString.textContent = number1;}
                     else break;
             }else{
-                if(outputString.textContent.length <= 18){
+                if(number2.length <= 18){
                     number2 = number2 + '.';
                     outputString.textContent = number2;}
                     else break;
             }
             break;
         case '+':
-            if(number1 !== '0'){
+            if(outputString.textContent == result){
+                number1 = result;
+                activeNumber = 2;
+                currentFunction = '+';
+            }
+            else if(activeNumber == 2){
+                number1 = Number(number1) + Number(number2);
+                outputString.textContent = number1;
+                number2 = '';
+            }
+            else{
                 currentFunction = '+';
                 activeNumber = 2;
             }
             break;
         case '-':
-            if(number1 !== '0'){
+            if(outputString.textContent == result){
+                number1 = result;
+                activeNumber = 2;
+                currentFunction = '-';
+            }
+            else if(activeNumber == 2){
+                number1 = Number(number1) - Number(number2);
+                outputString.textContent = number1;
+                number2 = '';
+            }
+            else{
                 currentFunction = '-';
                 activeNumber = 2;
             }
             break;
         case '*':
-            if(number1 !== '0'){
+            if(outputString.textContent == result){
+                number1 = result;
+                activeNumber = 2;
+                currentFunction = '*';
+            }
+            else if(activeNumber == 2){
+                number1 = Number(number1) * Number(number2);
+                outputString.textContent = number1;
+                number2 = '';
+            }
+            else {
                 currentFunction = '*';
                 activeNumber = 2;
             }
             break;
         case '/':
-            if(number1 !== '0'){
+            if(outputString.textContent == result){
+                number1 = result;
+                activeNumber = 2;
+                currentFunction = '/';
+            }
+            else if(activeNumber == 2){
+                number1 = Number(number1) / Number(number2);
+                outputString.textContent = number1;
+                number2 = '';
+            }
+            else{
                 currentFunction = '/';
                 activeNumber = 2;
             }
             break;
         case '=':
-            switch(currentFunction){
-            case "+":
-                let result1 = String(Number(number1)+Number(number2));
-                activeNumber = 1;
-                outputString.textContent = result1;
-                number1 = '';
-                number2 = '';
-                currentFunction ='';
-                break;
-            case "-":
-                let result2 = String(Number(number1)-Number(number2));
-                activeNumber = 1;
-                outputString.textContent = result2;
-                number1 = '';
-                number2 = '';
-                currentFunction ='';
-                break;
-            case "/":
-                let result3 = String(Number(number1)/Number(number2));
-                activeNumber = 1;
-                outputString.textContent = result3;
-                number1 = '';
-                number2 = '';
-                currentFunction ='';
-                break;
-            case "*":
-                let result4 = String(Number(number1)*Number(number2));
-                activeNumber = 1;
-                if(number2 == '0' || number2 == ''){
-                    outputString.textContent = Nan;
-                    number1 = '';
-                    number2 = '';
-                    currentFunction ='';
-                }else
-                outputString.textContent = result4;
-                number1 = '';
-                number2 = '';
-                currentFunction ='';
-                break;
+            operate(number1, number2, currentFunction);
         }         
     }
-}
+
 
 const clear = () => {
     if(activeNumber == 1){
